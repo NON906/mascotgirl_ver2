512d2296b9ad18dbc00151a4492587dcb1d0f69b
NON906 <mumeigames@gmail.com>
2024-11-11 14:18:34 +0900

    アンインストールの修正

--- bat/uninstall.bat
+++ bat/uninstall.bat
@@ -1,11 +1,25 @@
 @echo off
 setlocal
+
+call "%USERPROFILE%\miniconda3\condabin\conda" activate mascotgirl_ver2
+cd mascotgirl_ver2
+python uninstall.py
+cd ..
+call "%USERPROFILE%\miniconda3\condabin\conda" deactivate
+
 rd /S /Q mascotgirl_ver2
-call "%USERPROFILE%\miniconda3\condabin\conda" remove -n mascotgirl_ver2 --all -y
-if EXIST "%~dp0.installed\.miniconda" (
-    powershell -Command "Start-Process -Wait %USERPROFILE%\miniconda3\Uninstall-Miniconda3.exe /S"
+
+if NOT EXIST ".miniconda_uninstall" (
+    call "%USERPROFILE%\miniconda3\condabin\conda" remove -n mascotgirl_ver2 --all -y
 )
+if EXIST ".miniconda_uninstall" (
+    if EXIST "%~dp0.installed\.miniconda" (
+        powershell -Command "Start-Process -Wait %USERPROFILE%\miniconda3\Uninstall-Miniconda3.exe /S"
+    )
+)
+
 rd /S /Q .installed
+del /Q ".miniconda_uninstall"
 del /Q "run.bat"
 del /Q "run_local_net.bat"
 del /Q "run_cloudflare.bat"
---
+++ uninstall.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import subprocess
+
+if __name__ == "__main__":
+    output_str = subprocess.run(['conda', 'env', 'list'], capture_output=True, text=True).stdout
+    env_count = output_str.count('miniconda3')
+    if env_count == 2:
+        with open('../.miniconda_uninstall', 'w') as o:
+            o.write('')
